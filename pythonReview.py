a7578f0025276481a874b28dd0c32d20e1e8d424
faridja23-meet <109908216+faridja23-meet@users.noreply.github.com>
2022-07-25 18:42:24 +0300

    completed

--- pythonReview.py
+++ pythonReview.py
@@ -1,3 +1,31 @@
-def create_youtube_video(title,description):
-	New_Vid = {"title":title, "description": description, "likes": likes, "dislikes": dislikes, "comments": {"Username": comments}} 
-	return (New_Vid)
+def createYouTubeVideo(title, description):
+    return {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments" : {}}
+
+def like(video):
+    video["likes"] += 1
+    return video
+
+def dislike(video):
+    video["dislikes"] += 1
+    return video
+
+def addComment(video, username, comment):
+    video["comments"][username] = comment
+    return video
+
+bob = createYouTubeVideo("whatever", "skdnfkl")
+# print(bob)
+# like(bob)
+# dislike(bob)
+# like(bob)
+# print(bob)
+# addComment(bob,"farid","anything")
+# print(bob)
+# addComment("b")
+
+
+ 
+for i in range (496):
+	like(bob)
+
+print(bob)
